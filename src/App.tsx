328907d305b6bbace82a04568383e5ab37d7e136
ankurdas20 <137564408+ankurdas20@users.noreply.github.com>
2026-01-03 04:41:34 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import { User } from '@supabase/supabase-js'
-import { supabase } from './supabase/client' // Adjust this path to where your client is
+import { supabase } from './integrations/supabase/client' // Adjust this path to where your client is
 
 function App() {
   const [user, setUser] = useState<User | null>(null)
